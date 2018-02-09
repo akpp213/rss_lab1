6528c36bb58041fef6126078a7b2f02e46e8b9f6
Alex <knapping@mit.edu>
2018-02-09 08:18:23 -0500

    Add parameterization to open_space_publisher and fake_scan_publisher

--- rss_lab1/src/ros_exercises/fake_scan_publisher.py
+++ rss_lab1/src/ros_exercises/fake_scan_publisher.py
@@ -7,19 +7,19 @@ from sensor_msgs.msg import LaserScan
 
 def fake_scan_publisher():
     rospy.init_node('fake_scan_publisher')
-    pub = rospy.Publisher('fake_scan', LaserScan, queue_size=10)
-    rate = rospy.Rate(20) # 20hz
+    pub = rospy.Publisher(rospy.get_param("~pubs_topic", "fake_scan"), LaserScan, queue_size=10)
+    rate = rospy.Rate(rospy.get_param("~pubs_rate", 20)) # 20hz
     while not rospy.is_shutdown():
 	current_time = rospy.Time.now()
 	scan = LaserScan()
 	scan.header.stamp = current_time
 	scan.header.frame_id = "base_link"
-	scan.angle_min = -2*math.pi/3
-	scan.angle_max = 2*math.pi/3
-	scan.angle_increment = math.pi/300
+	scan.angle_min = rospy.get_param("~angle_min",-2*math.pi/3)
+	scan.angle_max = rospy.get_param("~angle_max",2*math.pi/3)
+	scan.angle_increment = rospy.get_param("~angle_increment",math.pi/300)
 	scan.scan_time = 1.0/20
-	scan.range_min = 1.0
-	scan.range_max = 10.0
+	scan.range_min = rospy.get_param("~range_min",1.0)
+	scan.range_max = rospy.get_param("~range_max",10.0)
 	num_readings = int((scan.angle_max - scan.angle_min) // scan.angle_increment) + 1
 	scan.ranges = [0] * num_readings
 	for i in range(0, num_readings):
@@ -27,6 +27,8 @@ def fake_scan_publisher():
         #rospy.loginfo(scan)
 	pub.publish(scan)
         rate.sleep()
+
+	
 
 if __name__ == '__main__':
     try:
--- rss_lab1/src/ros_exercises/open_space_publisher.py
+++ rss_lab1/src/ros_exercises/open_space_publisher.py
@@ -10,7 +10,7 @@ def callback(data):
      space = OpenSpace()
      longest = max(data.ranges)
      angle = data.angle_min + data.angle_increment*data.ranges.index(longest)
-     pub = rospy.Publisher('open_space', OpenSpace, queue_size = 10)
+     pub = rospy.Publisher(rospy.get_param("pub_topic",'open_space'), OpenSpace, queue_size = 10)
      space.distance = longest
      space.angle = angle
      rospy.loginfo(longest)
@@ -21,7 +21,7 @@ def open_space_publisher():
 
     rospy.init_node('open_space_publisher')
 
-    rospy.Subscriber('fake_scan', LaserScan, callback)
+    rospy.Subscriber(rospy.get_param("subs_topic",'fake_scan'), LaserScan, callback)
 
     # spin() simply keeps python from exiting until this node is stopped
     rospy.spin()
