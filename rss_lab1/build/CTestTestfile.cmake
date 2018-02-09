6fbf7b417532beb4ce7e75b5c988c02b1b2dad01
Alex <knapping@mit.edu>
2018-02-09 01:12:33 -0500

    create catkin workspace rss_lab1

---
+++ rss_lab1/build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/racecar/rss_lab1/rss_lab1/src
+# Build directory: /home/racecar/rss_lab1/rss_lab1/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
